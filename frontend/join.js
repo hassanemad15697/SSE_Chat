61f453ecea1af34052771596937ee9f5d7fdb362
Hassan Askar <hassan.askar@red.software.systems>
2023-10-02 16:33:34 +0200

    remove useless front end from project

--- frontend/join.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('joinForm').addEventListener('submit', function(e) {
-    e.preventDefault();
-    const username = document.getElementById('username').value;
-    // Store the username in localStorage
-    localStorage.setItem('username', username);
-    // Redirect to the chat page
-    window.location.href = 'chat.html';
-});
